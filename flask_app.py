bf723a69e86e53d5d7249ab0a612933ef86687d5
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2024-01-08 19:22:58 +0800

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,14 @@
 
 # A very simple Flask Hello World app for you to get started with...
-
+from flask_cors import CORS,cross_origin
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+app.secret_key="blebleblebleble"
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/_test_')
 def index():
