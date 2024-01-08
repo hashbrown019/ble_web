ff2eb1f8164278b54c70cedc699fe355a9a27c25
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2024-01-08 19:26:12 +0800

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -10,7 +10,7 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['JSON_SORT_KEYS'] = False
 
-@app.route('/_test_')
+@app.route('/_test_',methods=["POST","GET"])
 def index():
     return render_template("home.html")
 
